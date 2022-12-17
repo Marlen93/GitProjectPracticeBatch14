5875efa9d6aa5cc71f684c9c6e3a05ea97da4fa6
Marlen93 <marlen1993.mm@gmail.com>
2022-12-17 10:58:55 -0500

    version2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First Version Is complete");
+        System.out.println(" Version Is complete");
     }
 }
